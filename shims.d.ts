d09727bce678513a09ae495bae67a078a1fbb293
LION-YAN\Lion <Lion_yan@outlook.com>
2019-08-28 16:01:59 +0800

    no message

--- shims.d.ts
+++ shims.d.ts
@@ -10,15 +10,21 @@ declare namespace IR {
      * button pushed.
      */
     //% blockId=ir_received_left_event
-    //% block="on |%btn| button pressed" shim=IR::onPressEvent
-    function onPressEvent(btn: RemoteButton, body: () => void): void;
+    //% block="on |%btn| button pressed" 
+    //% shim=IR::onPressEvent
+    function onPressEvent(btn: RemoteButton, body:Action): void{
+		return
+	}
 
     /**
      * initialises local variablesssss
      */
     //% blockId=ir_init
-    //% block="connect ir receiver to %pin" shim=IR::init
-    function init(pin: Pins): void;
+    //% block="connect ir receiver to %pin" 
+    //% shim=IR::init
+    function init(pin: Pins): void{
+         return
+	}
 }
 
 // Auto-generated. Do not edit. Really.
