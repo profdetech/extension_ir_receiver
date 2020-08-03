f2a68fcbfb7a224f6e200d299253759c45dcbdc7
ELECFREAKS.Admin <support@elecfreaks.com>
2020-08-03 19:47:06 +0800

    更新 main.ts

--- main.ts
+++ main.ts
@@ -8,7 +8,6 @@ namespace IR {
   * initialization
   */
   //% blockId=ir_init
-  //% blockGap=20 weight=90
   //% block="connect ir receiver to %pin"
   //% shim=IR::init
   export function init(pin: Pins): void {
@@ -19,7 +18,6 @@ namespace IR {
   * button pushed.
   */
   //% blockId=ir_received_event
-  //% blockGap=20 weight=70
   //% block="on |%btn| button pressed"
   //% shim=IR::onPressEvent
   export function onPressEvent(btn: RemoteButton, body:Action): void {
