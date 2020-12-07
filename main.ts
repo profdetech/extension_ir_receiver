e30417590730e6a8d819240967204311ac0943d6
profdetech <emmanuel.mouronval@gmail.com>
2020-12-07 10:39:48 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -5,7 +5,7 @@
 //% icon="\uf1eb"
 namespace IR {
   /**
-     *  Lorsque le bouton de la telecommande du robot mbot est appuyé. Attention à initialiser le port du recepteur infrarouge.
+     *   initialiser infrarouge (portail automatique sur P13)
    */
   //% blockId=ir_init
   //% block="Initialiser le capteur infrarouge sur %pin"
@@ -16,7 +16,7 @@ namespace IR {
   }
 
   /**
-     * initialiser infrarouge (portail automatique sur P13)
+     * Lorsque le bouton de la telecommande du robot mbot est appuyé. Attention à initialiser le port du recepteur infrarouge.
      */
   //% blockId=ir_received_event
   //% block="Lorsque le signal du bouton |%btn| de la télécommande est reçu"
